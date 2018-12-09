8b628529ec42ab399785835f16b8a93a3acc5b7e
David Harding <davhar@ntdev.microsoft.com>
2018-12-08 20:15:19 -0800

    Initial checkin

---
+++ FrontEnd/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrontEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("node")]
+        public ActionResult<string> Get()
+        {
+            return Environment.MachineName;
+        }
+    }
+}
